21dea8dcfaee8fafbefe40d3df410c469efa8c5d
mateusaugust <mateusasilva033@gmail.com>
2023-11-21 19:22:07 -0300

    final Menu

--- ls/src/App.tsx
+++ ls/src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+import ConfirmGoOut from "./Modals/ConfirmGoOut";
+import LateralMenu from "./components/LateralMenu";
 import Register from "./components/Register";
 
 function App() {
 
+  const [open, setOpen] = useState(false);
+
   return (
    <>
-    <Register/>
+    <LateralMenu setOpen={setOpen}/>
+    <ConfirmGoOut setOpen={setOpen} open={open}/>
    </>
   );
 }
---
+++ ls/src/components/LateralMenu.tsx
@@ -0,0 +1,62 @@
+
+import React, { useState } from 'react'
+import { Box,  Grid, TextField, Button, Typography } from '@mui/material';
+
+type TlateralMenu = {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LateralMenu = ({setOpen}:TlateralMenu) => {
+  const options = ["Todos Chamados", "Abrir Chamados"];
+  const [section, setSelection] = useState(0);
+
+  const handleClickMenu = (index : number) =>{
+    setSelection(index);
+  }
+
+  return (
+    <Box sx={{borderRight:'1px solid #E0E0E0', width:'250px', height:'100vh', display:"flex", justifyContent:"space-between", flexDirection:"column" }}>
+      <Grid container >
+        <Grid item xs={12} sx={{display:"flex", justifyContent:"center", mt:"20px", borderBottom:'1px solid #E0E0E0', pb:"20px"}}>
+          <img src="https://olimaquinastratores.com.br/wp-content/uploads/2018/10/marca.png" style={{width:"100px"}}/>
+        </Grid>
+
+        
+        {options.map((text,index)=>(
+          <Grid item xs={12} >
+            <Box sx={{
+              mt: index === 0 ? "15px" : "0px",
+              display:"flex",
+              justifyContent:"center",
+              alignItems:"center",
+              height:'50px',
+              backgroundColor: section === index ? "#E0E0E0" : "white" ,
+              '&:hover':{backgroundColor:'#E0E0E0'}}}
+
+              onClick = {()=> handleClickMenu(index)}
+              
+              >
+              {text}
+            </Box>
+          </Grid>
+        ))} 
+      </Grid>
+
+      <Box sx={{padding:"20px", display:"flex", justifyContent:"center", alignItems:'center'}}> 
+        <Button variant="outlined" size="medium"  onClick={()=> setOpen(true)}
+          sx={{
+            backgroundColor:"#ED1C24", 
+            color:"white", 
+            borderColor:"#ED1C24",
+
+            '&:hover':{backgroundColor:"#1E3B6F", borderColor:"#1E3B6F"}
+          }} 
+        >
+          Sair
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default LateralMenu
