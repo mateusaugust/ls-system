64af4aafc243b765e72d4212102ca2f41f9f8738
Mateus Augusto <73893960+mateusaugust@users.noreply.github.com>
2023-11-21 19:36:17 -0300

    Menu lateral desabilitado (#9)
    * Menu lateral desabilitado
    
    * tela de registro desabilitada
    
    * comentario

--- ls/src/App.tsx
+++ ls/src/App.tsx
@@ -4,13 +4,28 @@ import LateralMenu from "./components/LateralMenu";
 import Register from "./components/Register";
 
 function App() {
+  //Se for mostrar a tela de Registro, comente todas linhas que não seja referente a ela.
+  //Caso for mostrar o menu lateral, comente todas as linhas que nao seja referente a ele.
 
-  const [open, setOpen] = useState(false);
+
+  //Para habilitar o menu lateral tire os comentarios da const abaixo,
+  //dos componentes LateralMenu e ConfirmGoOut
+  //const [open, setOpen] = useState(false);
 
   return (
    <>
-    <LateralMenu setOpen={setOpen}/>
-    <ConfirmGoOut setOpen={setOpen} open={open}/>
+    {
+      //Para Habilitar a Tela de Registro tire as barras de comentario do componente Register
+      //<Register/>
+    }
+
+
+    {
+      //Menu lateral
+      //<LateralMenu setOpen={setOpen}/>
+      //<ConfirmGoOut setOpen={setOpen} open={open}/>
+    }
+
    </>
   );
 }
