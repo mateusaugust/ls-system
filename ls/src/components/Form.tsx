33d0829e8e7e762d5bbf7c84d888fd44346a56f2
Thiago Dias <111087294+ThiagoBDias@users.noreply.github.com>
2023-12-13 14:55:28 -0300

    Alteração de tabela e conectado com o menu lateral (#14)

--- ls/src/components/Form.tsx
+++ ls/src/components/Form.tsx
@@ -1,96 +1,98 @@
-import React, { useState } from 'react';
-import { Box, Grid, TextField, Autocomplete } from '@mui/material';
+  import React, { useState } from 'react';
+  import { Box, Grid, TextField, Autocomplete } from '@mui/material';
 
-const Form = () => {
-  const [manualDate, setManualDate] = useState('');
+  const Form = () => {
+    const [manualDate, setManualDate] = useState('');
 
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
+    return (
       <Box
         sx={{
-          position: 'relative',
-          width: 600, 
-          height: 400,
-          bgcolor: 'background.paper',
-          p: 4,
-          borderRadius: '25px',
           display: 'flex',
-          flexDirection: 'column',
           alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
-        <Grid container spacing={4}>
-          <Grid item xs={6}sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-            <TextField id="outlined-password-input" label="Numeração" type="text" />
-          </Grid>
+        <Box
+          sx={{
+            position: 'relative',
+            width: 600,
+            height: 400,
+            bgcolor: 'background.paper',
+            p: 4,
+            borderRadius: '25px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={6} sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <TextField id="outlined-password-input" label="Numeração" type="text" />
+            </Grid>
 
-          <Grid item xs={6}sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-            <TextField id="outlined-password-input" label="Usuario" type="text" sx={{ width: '80%' }} />
-          </Grid>
+            <Grid item xs={12} md={6} sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <TextField id="outlined-password-input" label="Usuário" type="text" sx={{ width: '80%' }} />
+            </Grid>
 
-          <Grid item xs={12} md={4} sx={{ width: '100%' }}>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={prioridade}
-              sx={{ width: '100%' }}
-              renderInput={(params) => <TextField {...params} label="Prioridade" />}
-            />
-          </Grid>
+            <Grid item xs={12} md={6} sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={prioridade}
+                sx={{ width: '80%' }}
+                renderInput={(params) => <TextField {...params} label="Prioridade" />}
+              />
+            </Grid>
 
-          <Grid item xs={12} md={4} sx={{ width: '100%' }}>
-            <TextField
-              id="manual-date-input"
-              label=" "
-              type="date"
-              value={manualDate}
-              sx={{ width: '100%' }}
-            />
-          </Grid>
+            <Grid item xs={12} md={6} sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <TextField
+                id="manual-date-input"
+                label=""
+                type="date"
+                value={manualDate}
+                onChange={(e) => setManualDate(e.target.value)}
+                sx={{ width: '80%' }}
+              />
+            </Grid>
 
-          <Grid item xs={12} md={4} sx={{ width: '100%' }}>
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={Status}
-              sx={{ width: '100%' }}
-              renderInput={(params) => <TextField {...params} label="Status" />}
-            />
-          </Grid>
+            <Grid item xs={12} md={12} sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={Status}
+                sx={{ width: '90%' }}
+                renderInput={(params) => <TextField {...params} label="Status" />}
+              />
+            </Grid>
 
-          <Grid item xs={12} sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-            <TextField
-              id="filled-multiline-static"
-              label="Informe seu problema"
-              multiline
-              rows={5}
-              defaultValue=""
-              variant="filled"
-              sx={{ width: '100%' }}
-            />
+            <Grid item xs={12} sx={{ width: '80%', display: 'flex', justifyContent: 'center' }}>
+              <TextField
+                id="filled-multiline-static"
+                label="Informe seu problema"
+                multiline
+                rows={5}
+                defaultValue=""
+                variant="filled"
+                sx={{ width: '90%' }}
+              />
+            </Grid>
           </Grid>
-        </Grid>
+        </Box>
       </Box>
-    </Box>
-  );
-};
+    );
+  };
 
-const Status = [
-  { label: 'Espera', year: 1994 },
-  { label: 'Andamento', year: 1972 },
-  { label: 'Concluída', year: 1974 },
-];
+  const Status = [
+    { label: 'Espera' },
+    { label: 'Andamento' },
+    { label: 'Concluída' },
+  ];
 
-const prioridade = [
-  { label: 'Urgente', year: 1994 },
-  { label: 'Média', year: 1972 },
-  { label: 'Tranquila', year: 1974 },
-];
+  const prioridade = [
+    { label: 'Urgente' },
+    { label: 'Média' },
+    { label: 'Tranquila' },
+  ];
 
-export default Form;
+  export default Form;
+    
--- ls/src/index.tsx
+++ ls/src/index.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ListAlter from './components/ListAlter';
+import Form from './components/Form';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
